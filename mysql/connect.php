8385f14e991267615db71a239589e65158f862e0
Kosiarz <Kosiarz>
2022-11-07 18:17:53 +0100

    connecting to the mysql database and get data

---
+++ mysql/connect.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost:3307";
+$username = "root";
+$password = "root";
+$dbname = "mysqlbase";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, name FROM USER";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
