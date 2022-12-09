1119476bda4c968488e6199f3f83581b23f03b56
Kosiarzz <patryk.pasciakk@gmail.com>
2022-12-09 16:14:01 +0100

    Mysql zapytania

--- app/Http/Controllers/MySqlController.php
+++ app/Http/Controllers/MySqlController.php
@@ -16,13 +16,39 @@ class MySqlController extends Controller
     {
         //DB::connection('mysql')->select('select * from users where active = ?', [1]);
 
-        $results = DB::connection('mysql')->select(
-                'SELECT * 
-                FROM employees e, salaries s
-                WHERE e.id = 1
-                AND e.id = s.employee_id
-            ');
+        // $results = DB::connection('mysql')->select(
+        //         'SELECT * 
+        //         FROM employees e, salaries s, titles t
+        //         WHERE e.id = t.employee_id
+        //         AND e.id = s.employee_id
+        //     ');
 
+
+        //Ile wypłat dostał pracownik i jaką największą
+        $results = DB::connection('mysql')->select(' 
+            SELECT first_name, last_name, X.*
+            FROM employees E, (
+            SELECT employee_id, COUNT(*) as salaries, MAX(salary) as max_salary
+            FROM salaries
+            GROUP BY employee_id) X
+            WHERE E.id = X.employee_id
+        ');
+
+        //Pierwszy tytuł
+        $results = DB::connection('mysql')->select(' 
+            SELECT first_name, last_name, X.*, Y.*
+            FROM employees E, (
+            SELECT employee_id, COUNT(*) as salaries, MAX(salary) as max_salary
+            FROM salaries
+            GROUP BY employee_id) X, (
+            SELECT employee_id, title
+            FROM titles
+            ORDER BY "DESC") Y
+            WHERE E.id = X.employee_id
+            AND E.id = Y.employee_id
+        ');
+     
+        dd($results);        
         return view('testmysql', [
             'results' => $results,
         ]);
